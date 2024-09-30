8b8e16eeb9ab46f90d4a396b3a4ee4d3c4e7ace1
shalompeter <shazzywise@gmail.com>
2024-09-30 15:04:43 +0100

    commit message

---
+++ database/migrations/2024_09_30_122800_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained(); 
+            $table->decimal('total_amount', 8, 2);
+            $table->dateTime('order_date');
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
